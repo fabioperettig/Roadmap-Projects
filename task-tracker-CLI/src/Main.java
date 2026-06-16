213f314a548b1d525bd3c896e6079e150099e174
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2026-06-16 18:02:50 +0100

    Services Class implements

--- task-tracker-CLI/src/Main.java
+++ task-tracker-CLI/src/Main.java
@@ -1,7 +1,10 @@
 import fabioperettig.tt.model.Task;
+import fabioperettig.tt.service.TaskManager;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         /// Cash the paycheck;
         /// Return a overdue book;
@@ -9,6 +12,18 @@ public class Main {
         /// Cast your ballot;
         /// Buy some milk;
         /// Get a Christmas Tree;
+
+        TaskManager tManager = TaskManager.build();
+
+        Task tsk1 = new Task("Get a Christmas Tree");
+        Task tsk2 = new Task("Buy some milk");
+        Task tsk3 = new Task("Get Gary's autograph");
+
+        tManager.saveTask(tsk1);
+        tManager.saveTask(tsk2);
+        tManager.saveTask(tsk3);
+
+
 
     }
 }
--- task-tracker-CLI/src/fabioperettig/tt/service/TaskManager.java
+++ task-tracker-CLI/src/fabioperettig/tt/service/TaskManager.java
@@ -1,4 +1,27 @@
 package fabioperettig.tt.service;
+import fabioperettig.tt.model.Task;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class TaskManager {
+
+    private TaskManager() {
+    }
+
+    public static TaskManager build(){
+        return new TaskManager();
+    }
+
+    private static final Path FILE = Path.of("myTasks.json");
+
+    public void saveTask (Task task) throws IOException {
+        Files.writeString(FILE, task.toJson() +
+                        System.lineSeparator(),
+                        StandardOpenOption.CREATE,
+                        StandardOpenOption.APPEND);
+    }
+
 }
